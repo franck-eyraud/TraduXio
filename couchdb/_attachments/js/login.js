49d1c770d8ad61558a4eb07559d5380466a07f8e
Franck Eyraud <franck+github@yrnm.net>
2014-09-20 20:16:40 +0200

    FEATURE: Login form, user can authentify.

---
+++ couchdb/_attachments/js/login.js
@@ -0,0 +1,60 @@
+function session() {
+  $.ajax({
+    url:"/_session",
+    dataType:"json"
+  }).done(function(result){
+    updateUserInfo(result.userCtx);
+  });
+};
+
+function updateUserInfo(ctx) {
+  var sessionInfo=$("#session-info");
+  if (ctx.name) {
+    var userSpan=$("<span>").addClass("user").text(ctx.name);
+    var logoutSpan=$("<span>").addClass("logout").text("logout").on("click",function(){
+      logout();
+    });
+    sessionInfo.empty().append(userSpan).append(" - ").append(logoutSpan);
+  } else {
+    var form=$("<form>").addClass("login");
+    var username=$("<input>").addClass("username").attr("placeholder","username");
+    var password=$("<input>").addClass("password").attr("type","password").attr("placeholder","password");
+    var go=$("<input>").addClass("go").attr("type","submit").val("login");
+    form.append(username).append(password).append(go).on("submit",function(e) {
+      e.preventDefault();
+      var name=username.val();
+      var passwd=password.val();
+      if (name && passwd)
+        login(username.val(),password.val()).fail(function() {
+          username.add(password).addClass("bad");
+        });
+      else username.add(password).addClass("bad");
+    });
+    sessionInfo.empty().append(form);
+  }
+}
+
+function logout() {
+  $.ajax({
+    url:"/_session",
+    dataType:"json",
+    type:"DELETE"
+  }).done(function(result){
+    updateUserInfo(result);
+  });  
+}
+
+function login(name,password) {
+ return $.ajax({
+    url:"/_session",
+    dataType:"json",
+    type:"POST",
+    data:"name="+encodeURIComponent(name)+"&password="+encodeURIComponent(password)
+  }).done(function(result){
+    updateUserInfo(result);
+  });
+}
+
+$(document).ready(function() {
+  session();
+});
