a0666da34b1790d5e10cf1143349ca4269bf4ec8
Franck Eyraud <franck+github@yrnm.net>
2017-06-14 12:54:58 +0200

    FEATURE: Enforce privileges on User Interface.
    Hide works and translations with no access, do not allow editing to
    read-only ones.

--- couchdb/shows/work.js
+++ couchdb/shows/work.js
@@ -27,88 +27,96 @@ function(o, req) {
   }
   var data = {
     id: o._id,
-    seq:req.info.update_seq,
-    work_title: o.title,
-    display_work_title: o.title?o.title:i18n["i_no_title"],
-    work_creator: o.creator,
-    display_work_creator: o.creator?o.creator:i18n["i_no_author"],
-    work_language: o.language,
-    original: o.text ? true : false,
-    date:o.date,
-    lines: getTextLength(),
     headers: [],
     units: [],
     rows:[],
+    lines:1,
     i18n:i18n
-  };
+  }
+  if (Traduxio.canAccess(o)) {
+    data.seq=req.info.update_seq;
+    data.work_title=o.title;
+    data.display_work_title=o.title?o.title:i18n["i_no_title"];
+    data.work_creator=o.creator;
+    data.display_work_creator=o.creator?o.creator:i18n["i_no_author"];
+    data.work_language=o.language;
+    data.original=o.text ? true : false;
+    data.date=o.date;
+    data.lines=getTextLength();
+    data.canEdit=Traduxio.canEdit();
+    data.canAccess=true;
 
-  if (!newWork) {
-    var hexapla = new Hexapla();
-    var edited_versions=req.query.edit ? req.query.edit.split("|") : [];
-    var opened_versions=req.query.open ? req.query.open.split("|") : [];
-    if (o.text) {
-      hexapla.addVersion({
-        id: "original",
-        text: o.text
-      });
-      data.headers.push({
-        version: "original",
-        is_original: true,
-        title: o.title,
-        language: o.language,
-        date: o.date,
-        raw:o.text,
-        creativeCommons: o.creativeCommons,
-        edited: (edited_versions.indexOf("original")!=-1),
-        opened: (opened_versions.indexOf("original")!=-1)
-      });
+    if (!newWork) {
+      var hexapla = new Hexapla();
+      var edited_versions=req.query.edit ? req.query.edit.split("|") : [];
+      var opened_versions=req.query.open ? req.query.open.split("|") : [];
+      if (o.text) {
+        hexapla.addVersion({
+          id: "original",
+          text: o.text
+        });
+        data.headers.push({
+          version: "original",
+          is_original: true,
+          title: o.title,
+          language: o.language,
+          date: o.date,
+          raw:o.text,
+          creativeCommons: o.creativeCommons,
+          edited: (edited_versions.indexOf("original")!=-1),
+          opened: (opened_versions.indexOf("original")!=-1)
+        });
+      }
+
+      for (var t in o.translations) {
+        var translation = o.translations[t];
+        hexapla.addVersion({
+          id: t,
+          text: Traduxio.canAccess(translation) ? translation.text : [""],
+        });
+        data.headers.push({
+          version:t,
+          title: translation.title,
+          work_creator: translation.creator || "",
+          creator: t,
+          language: translation.language || "",
+          date: translation.date,
+          raw:Traduxio.canAccess(translation) ? translation.text : [],
+          creativeCommons: translation.creativeCommons,
+          canAccess: Traduxio.canAccess(translation),
+          opened: Traduxio.canAccess(translation) && (opened_versions.indexOf(t)!== -1),
+          canEdit: Traduxio.canEdit(translation),
+          edited: Traduxio.canEdit(translation) && (edited_versions.indexOf(t)!== -1)
+
+        });
+      }
+      data.rows=hexapla.getRows();
     }
 
-    for (var t in o.translations) {
-      var translation = o.translations[t];
-      hexapla.addVersion({
-        id: t,
-        text: translation.text
-      });
-      data.headers.push({
-        version:t,
-        title: translation.title,
-        work_creator: translation.creator || "",
-        creator: t,
-        language: translation.language || "",
-        date: translation.date,
-        raw:translation.text,
-        creativeCommons: translation.creativeCommons,
-        edited: (edited_versions.indexOf(t)!== -1),
-        opened: (opened_versions.indexOf(t)!== -1)
-      });
+    data.page_title=data.work_creator+" : "+data.work_title;
+    data.language=data.work_language;
+    if (o.glossary) {
+      var glossary=o.glossary;
+    } else {
+      glossary={};
     }
-    data.rows=hexapla.getRows();
+    delete glossary.edits;
+    data.glossary=JSON.stringify(glossary);
+    data.notext=o.text ? false : true;
+    data.original=o.text ? true : (newWork ? true : false);
+    if (data.headers.length==1) {
+      data.justOneText=true;
+      data.version=data.headers[0].version
+    }
   }
 
+  data.prefix="..";
   data.name="work";
-  data.page_title=data.work_creator+" : "+data.work_title;
   data.css=true;
   data.script=true;
   data.scripts=["jquery.selection","jquery.ajax-retry","activity","jquery.highlight"];
   if (this.couchapp.traduxio.chat) data.scripts.push("chat");
   if (this.couchapp.traduxio.sessions) data.scripts.push("sessions");
-  log(this.couchapp.traduxio);
-  data.language=data.work_language;
-  data.prefix="..";
-  if (o.glossary) {
-    var glossary=o.glossary;
-  } else {
-    glossary={};
-  }
-  delete glossary.edits;
-  data.glossary=JSON.stringify(glossary);
-  data.notext=o.text ? false : true;
-  data.original=o.text ? true : (newWork ? true : false);
-  if (data.headers.length==1) {
-    data.justOneText=true;
-    data.version=data.headers[0].version
-  }
 
   return Mustache.to_html(this.templates.work, data, this.templates.partials);
 }
