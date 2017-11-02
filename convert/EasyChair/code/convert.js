483dd205d583b65c8e05c5d71dfd2bd63208e172
Franck Eyraud <franck+github@yrnm.net>
2017-11-02 21:26:31 +0100

    STASH: Convert script improvement.

--- convert/EasyChair/code/convert.js
+++ convert/EasyChair/code/convert.js
@@ -30,9 +30,12 @@ var db;
 
 var cookies={};
 
+console.log("Logging in on "+config.server+" as "+config.user);
+
 nano(config.server).auth(config.user,config.password,function (err, body, headers) {
   if (err) {
     console.log("error logging in");
+    console.log(err);
     return;
   }
 
@@ -70,6 +73,7 @@ function readFull() {
 
 var reverse_languages={
   "English":"en",
+  "Putonghua":"zh",
   "Chinese":"zh"
 };
 
@@ -80,27 +84,53 @@ function splitText(abstract) {
   //or if they are preceded by a word of one letter or a list of abbrieviation words,
   //plus the tailing punctuation characters and their following spaces.
   //Test RegExp : https://regexr.com/3h12o
-  var sentence=/((?:\W(\w|cf|ie|eg|etc\.*|Mrs|Miss|Dr))\.(?=\s)|[^\.\?\!]+?|[\!\?\.][^\s\!\?\.])+([\?\.\!]+\s*|$)/g;
-  return abstract.match(sentence)
+  var sentence=/((?:\W(\w|al|cf|eds|eg|ie|no|pp|qtd|vol|vs|etc\.*|Mrs|Miss|Dr))\.(?=\s)|[^\.\?\!]+?|[\!\?\.][^\s\!\?\.])+([\?\.\!]+\s*|$)/g;
+  return abstract.match(sentence);
 }
 
+var lock=false;
+
+function store(work, callback) {
+  if (!lock) {
+    lock=true;
+    console.log("taking lock");
+    db.insert(work,function(err,body) {
+      console.log("releasing lock");
+      lock=false;
+      callback(err,body);
+    });
+  } else {
+    setTimeout(store,5,work,callback);
+  }
+}
+
+var tostore=0,stored=0;
+
 function treatRow(row,callback) {
-  console.log("treat Row "+row["#"]);
+  //console.log("treat Row "+row["#"]);
   var work={};
   work.id="EasyChairImport-"+row["#"];
   work.title=row.title;
   work.creator=row.authors;
-  work.metadata={};
+  work.metadata=row;
   work.metadata.id=row["#"];
-  work.metadata.keywords=row.keywords.split("\n");
+  work.metadata.keywords=row.keywords ? row.keywords.split("\n") : [];
   work.metadata.original_text=row.abstract;
-  work.text=splitText(row.abstract);
+  work.text=row.abstract ? splitText(row.abstract) : [];
   work.date=row.date;
   work.language="en"; //default
   work.privileges={owner:config.user};
 
+  // Check if sentences are correctly split
+  // work.text.forEach(function(line,i) {
+  //   if (i>0 && line.length>0 && !line[0].match(/[A-Z]/)) {
+  //     console.log("#"+row["#"]+" "+work.creator+" "+i+": "+work.text[i-1]+"\n"+line);
+  //   }
+  // })
+  // return;
 
-  var form_fields=row["form fields"].split("\n");
+
+  var form_fields=row["form fields"] ? row["form fields"].split("\n") : [];
   //EasyChair form fields are one per line with syntax :
   //(form field label) form field value
   var extract=/\(([^)]*)\) (.*)/;
@@ -118,6 +148,17 @@ function treatRow(row,callback) {
       work.language=reverse_languages[language];
     }
   }
+  // if (work.metadata["Language of Presentation"]!="English") {
+  //   console.log("#"+row["#"]+" does not use English : "+work.metadata["Language of Presentation"]);
+  // }
+  // if (work.metadata["IATIS Policy"]!="Agree") {
+  //   console.log("#"+row["#"]+" did not accept IATIS Policy "+work.metadata["IATIS Policy"]);
+  // }
+  // if (row.decision!='accept' || work.metadata["IATIS Policy"]!="Agree") {
+  //   //console.log("not accepted, skip");
+  //   return callback();
+  // }
+  // console.log("#"+row['#']+" accepted, store");
   work.translations={};
   var new_trans={};
   var possible_languages=translation_languages.slice();
@@ -128,14 +169,17 @@ function treatRow(row,callback) {
   new_trans.text=new Array(work.text.length).fill("");
   new_trans.privileges={owner:config.user};
   work.translations["iatis"]=new_trans;
-  console.log(work);
+  //console.log(work);
+  //return callback();
+  tostore++;
   db.insert(work,function(err,body) {
     if (err) console.log(err);
     else {
-      console.log("stored "+work._id);
-      console.log(body);
+      stored++;
+      console.log("stored "+work.id+" as "+body.id);
+      //console.log(body);
     }
-    callback();
+    setTimeout(callback,100);
   });
 }
 
@@ -148,20 +192,27 @@ function readStep() {
     step:function(parsed,p) {
       try {
         rows++;
-        console.log("read row #"+rows);
+        //console.log("read row #"+rows);
         var row=parsed.data[0];
         if (row) {
-          p.pause();
+          var done=rows;
+          //p.pause();
           treatRow(row,function() {
-            p.resume();
+            //console.log("finished "+done);
           });
+          //p.resume();
         }
       } catch(e) {
         console.log(e);
       }
     },
     error:function(err) {
+      console.log("error");
       console.log(err);
+    },
+    complete:function () {
+      console.log("finished");
+      console.log("stored "+stored+" out of "+tostore);
     }
   });
 }
