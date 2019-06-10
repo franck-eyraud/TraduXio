eeac293edc568457eaeab266bd7c2a36f0de03cf
Franck Eyraud <franck+github@yrnm.net>
2019-06-10 10:48:59 +0200

    FIX: Deploy script actually refresh views.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ DATABASE=${1-http://localhost:5984/traduxio}
 log=deploy.log
 
 echo Deploying on $DATABASE
-couchapp push couchdb $DATABASE 2>&1 > $log
+couchapp push couchdb $DATABASE 2> $log
 DB=$(cat $log | tail -1)
 if [ $? -eq 0 ]; then
   echo Deployed on $DB
